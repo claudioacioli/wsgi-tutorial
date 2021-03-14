8dd32c4b0e8115aefeb96a0a02082c034c0f413b
ClaudioAcioli <claudio.acioli.silva@gmail.com>
2021-03-14 14:16:03 +0000

    refactor: Use simple functions

--- post_json.py
+++ post_json.py
@@ -1,13 +1,15 @@
 from wsgiref.simple_server import make_server
 import json
 
-
-def get_body(environ):
+def get_request_length(environ):
     try:
-        length = int(environ['CONTENT_LENGTH'] or 0)
+        return int(environ['CONTENT_LENGTH'] or 0)
     except ValueError:
-        length = 0
+        return 0
 
+
+def get_request_body(environ):
+    length = get_request_length(environ)
     if length:
         body = environ['wsgi.input'].read(length)
         return body.decode('utf-8')
@@ -15,9 +17,12 @@ def get_body(environ):
     return None
 
 
-def handle_json(environ):
-    body = get_body(environ) or '{}'
-    return json.loads(body)
+def handle_request_json(environ):
+    try:
+        body = get_request_body(environ) or '{}'
+        return json.loads(body)
+    except:
+        return {}
 
 
 def application (environ, start_response):
@@ -26,7 +31,7 @@ def application (environ, start_response):
    
     method = environ['REQUEST_METHOD']
     if method == 'POST' and environ['CONTENT_TYPE'] == 'application/json':
-        json_params = handle_json(environ)
+        json_params = handle_request_json(environ)
         print(json_params)
 
     headers = [
